2e93908f15fd8b3aa2adb5d7c5c743c9a2c64841
josef <josef.abdulkarim@hotmail.co.uk>
2025-08-11 19:23:28 +0100

    Added MySQL server with docker compose and allowed chicken_ui program saves, modifies and deletes records from the database for persistence.

--- chicken.py
+++ chicken.py
@@ -1,9 +1,13 @@
 class Chicken():
-    def __init__(self, name):
+    def __init__(self, name, chicken_id = 0):
         self.name = name
+        self.id = chicken_id
 
     def get_name(self):
         return self.name
+
+    def get_id(self):
+        return self.id
 
     def change_name(self, name):
         self.name = name
---
+++ db_connection.py
@@ -0,0 +1,37 @@
+import mysql.connector
+
+class DatabaseConnection:
+    def __init__(self, host, user, password, database, port = 3306):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.port = port
+        self.connection = None
+        self.cursor = None
+
+    def connect(self):
+        self.connection = mysql.connector.connect(
+            host = self.host,
+            user = self.user,
+            password = self.password,
+            database = self.database,
+            port = self.port
+        )
+        self.cursor = self.connection.cursor()
+
+    def load_from_database(self):
+        self.cursor.execute("SELECT * FROM chicken")
+        return self.cursor.fetchall()
+
+    def add_to_database(self, chicken):
+        self.cursor.execute(f"INSERT INTO chicken (chicken_name) VALUES ('{chicken.get_name()}')")
+        self.connection.commit()
+
+    def delete_from_database(self, chicken_id):
+        self.cursor.execute(f"DELETE FROM chicken WHERE chicken_id = {chicken_id}")
+        self.connection.commit()
+
+    def update_in_database(self, chicken_id, chicken_name):
+        self.cursor.execute(f"UPDATE chicken SET chicken_name = '{chicken_name}' WHERE chicken_id = {chicken_id}")
+        self.connection.commit()
